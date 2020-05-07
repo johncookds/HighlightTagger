917a2a003e6148697075a263bae687f13f0984ba
John Cook <johncookds@gmail.com>
2020-05-06 19:25:42 -0700

    collecting more data with each selection now, also can single tag and edit tag by icon click

--- background.js
+++ background.js
@@ -1,22 +1,30 @@
+var label = "Default"
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
-	console.log('messsage caught');
-	switch(request.message)
-	{
-		case 'setText':
-			window.seltext = request.data.text
-		break;
-		
-		default:
-			sendResponse({data: 'Invalid arguments'});
-		break;
-	}
-	console.log(request);
+
 	var jax = new XMLHttpRequest();
 	jax.open("POST","http://localhost:5000/text");
 	jax.setRequestHeader("Content-Type","application/json;charset=UTF-8");
-	jax.send(JSON.stringify({'url': request.data.url, 
-                   			'text': window.seltext}
+	jax.send(JSON.stringify({'text' : request.data.selection_text,
+                             'location' : request.data.location,
+                             'section_outertext' : request.data.section_outertext,
+                             'section_outerhtml' : request.data.section_outerhtml,
+                             'document_outertext' : request.data.document_outertext,
+                             'document_outerhtml' : request.data.document_outerhtml,
+                             'document_title' : request.data.document_title,
+                             'document_text' : request.data.document_text,
+                             'label' : label
+                             }
                             ));
 });
 
+
+chrome.browserAction.onClicked.addListener(function(tab) { 
+	var new_label = prompt("Please provide a label with which you would like to tag content?", label);
+	if (new_label!=null){
+		label = new_label
+		x="Name changed to: " + label;
+		alert(x);
+	}
+});
--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,23 @@
 document.addEventListener('mouseup',function(event)
 {
-    var sel = window.getSelection().toString();
-    console.log(event.target.baseURI);
-    var current_tab = event.target.baseURI;
-    if(sel.length)
-        chrome.runtime.sendMessage({'message':'setText',
-                                    'data' : {'text': sel,
-                                            'url': current_tab}});
+    console.log(event);
+    var selection_text = window.getSelection().toString();
+    var location = window.location;
+    var section_outertext = event.target.outerText;
+    var section_outerhtml = event.target.outerHTML;
+    var document_outertext = event.target.ownerDocument.body.outerText;
+    var document_outerhtml = event.target.ownerDocument.body.outerHTML;
+    var document_title = event.target.ownerDocument.body.title;
+    var document_text = event.target.ownerDocument.body.text;
+    if(selection_text.length)
+        chrome.runtime.sendMessage({'message' : 'setText',
+                                    'data' : {'text' : selection_text,
+                                            'location' : location,
+                                            'section_outertext' : section_outertext,
+                                            'section_outerhtml' : section_outerhtml,
+                                            'document_outertext' : document_outertext,
+                                            'document_outerhtml' : document_outerhtml,
+                                            'document_title' : document_title,
+                                            'document_text' : document_text
+                                        }});
 })
